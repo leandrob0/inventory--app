5a6c3eee848e140ff9aea34d5d25e4441f94ad5e
leandrob0 <leandro.bovino@gmail.com>
2022-02-11 12:22:19 -0300

    cors img-src self data fixed

--- app.js
+++ app.js
@@ -24,9 +24,16 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(helmet()); // Protects against vulnerabilities.
-app.use(compression()); //Compress all routes.
 app.use(cors());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"],
+    },
+  })
+);
+app.use(compression()); //Compress all routes.
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
